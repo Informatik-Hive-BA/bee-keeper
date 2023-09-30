c6eff05a04153c201130dfff7639b9c598b1917f
GJVothel <g.j.vothel@gmx.de>
2023-09-30 16:22:53 +0200

    added logging

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
 from dotenv import load_dotenv
 import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 load_dotenv()
 __token__ = os.getenv('TOKEN', '')
