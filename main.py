3f905176d2a6c10734770e41106a813843872e2f
GJVothel <73531445+GJVothel@users.noreply.github.com>
2023-11-14 09:51:25 +0100

    split logging files into days

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ import discord
 import logging
 from datetime import date
 
-today = date.today()
-
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='T'+str(today)+'discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='T'+str(date.today())+'discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
